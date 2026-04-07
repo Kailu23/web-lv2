e7f9f1afc2abf6849069ca2e8ca5c935a027b4d7
Marin Valenčak <valencakmarin@gmail.com>
2026-04-07 03:48:29 +0200

    Solver 3rd problem

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express');
-const app = express();
-
+const fs = require('fs')
+const path = require('path')
 const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.set("view engine", "ejs")
 app.use(express.static('public')); // "posluzuje" index.html
+
 // Automatski koristi sve iz mape public
 app.get('/', (req, res) => {
-res.send("Pozdrav sa Railway servera!");
+res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+app.get('/slike', (req, res) => {
+    const dataPath = path.join(__dirname, 'images.json');
+    const images = JSON.parse(fs.readFileSync(dataPath));
+
+    images.forEach((image, i) => {
+        image.id = `slika${i + 1}`;
+    });
+
+    res.render('slike', { images });
 });
 
 app.listen(PORT, () => {
-console.log("Server pokrenut na portu ${PORT}");
+    console.log("Server pokrenut na portu ${PORT}");
 });
